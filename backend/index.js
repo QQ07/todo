330ca5f22e93a19e43515bd369aeeccf24beea9a
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2024-01-16 12:25:22 +0530

    port 80

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors= require("cors")
+const cors = require("cors");
 const app = express();
-app.use(cors())
+app.use(cors());
+require("dotenv").config();
 const userRouter = require("./routes/user");
-
+const PORT = process.env.PORT || 80;
 app.use("/", userRouter);
 
-app.listen(3000, (err) => {
-  if (err) throw(err);
-    console.log("http://localhost:3000")
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log("running on PORT: "+PORT);
 });
