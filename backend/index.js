0d12b1158f93ecf3c9f95f55e35830a0429418f0
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2024-01-19 00:23:28 +0530

    add route in backend

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-require("dotenv").config();
+app.use(express.json())
+
 const userRouter = require("./routes/user");
+require("dotenv").config();
 const PORT = process.env.PORT || 80;
+
+
+
 app.use("/", userRouter);
 
 app.listen(PORT, (err) => {
   if (err) throw err;
-  console.log("running on PORT: "+PORT);
+  console.log("running on PORT: " + PORT);
 });
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,19 +3,21 @@ const todos = require("../db/models");
 
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-  res.send("Todos backend")
-})
+router.get("/", (req, res) => {
+  res.send("Todos backend");
+});
 router.get("/allTodos", async (req, res) => {
   const all = await todos.find();
   console.log(all);
   res.send(all);
 });
 
-router.get("/add", async (req, res) => {
-  const newTodo = req.body();
-  console.log(newTodo);
-
+router.post("/add", async (req, res) => {
+  console.log(req.body);
+  const todo = new todos(req.body);
+  const msg = await todo.save();
+  console.log(msg);
+  res.json({ msg: "added" });
 });
 
 module.exports = router;
